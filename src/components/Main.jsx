7f5d54314b668577734f6d843338ece96e450f13
nencheff <i.nencheff@gmail.com>
2026-03-09 07:41:14 +0200

    add state for component reset

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -4,11 +4,20 @@ import Questions from './Questions'
 
 export default function Main() {
     const [step, setStep] = React.useState(1)
+    const [quizKey, setQuizKey] = React.useState(0)
 
     return (
         <main>
             {step === 1 && <StartQuizz onNext={() => setStep(2)} />}
-            {step === 2 && <Questions onPrev={() => setStep(1)} />}
+            {step === 2 && (
+                <Questions
+                    key={quizKey}
+                    onPrev={() => {
+                        setStep(1)
+                        setQuizKey(prev => prev + 1)
+                    }}
+                />
+            )}
         </main>
     )
 }
