3b1fb227f4a404d17d98c0557b1c1d97bc400a61
nencheff <i.nencheff@gmail.com>
2026-03-09 06:59:08 +0200

    correct answers logic, styles

--- src/components/Questions.jsx
+++ src/components/Questions.jsx
@@ -4,34 +4,55 @@ import { questionsData } from '../data/questions'
 import he from 'he';
 
 export default function Questions(props) {
-    const [questions, setQuestions] = React.useState(questionsData)
+    const [questions, setQuestions] = React.useState(() =>
+        questionsData.map(q => ({
+            ...q,
+            answers: [
+                ...q.incorrect_answers,
+                q.correct_answer
+            ].sort(() => Math.random() - 0.5)
+        }))
+    )
+    
+    const [userAnswers, setUserAnswers] = React.useState({})
+        const btnClassName = "default lg"
+        const btnPlayValue = "Play again"
+        const btnCheckAnswersValue = "Check answers"
 
-        const btnClassName = "default xl"
-        const btnValue = "Play again"
+    const [checked, setChecked] = React.useState(false)
 
-        // React.useEffect(() => {
-        // fetch("https://opentdb.com/api.php?amount=5&type=multiple")
-        //     .then(res => res.json())
-        //     .then(data => setQuestionsData(data))
-        // }, [])
+    const score = questions.reduce((total, question) => {
+        if (userAnswers[question.question] === question.correct_answer) {
+            return total + 1
+        }
+        return total
+    }, 0)
 
     const generateQuestions = questions.map((question, qIndex) => {
-        const answers = [
-            ...question.incorrect_answers,
-            question.correct_answer
-        ].sort(() => Math.random() - 0.5)
-
         return (
             <div className='question' key={qIndex}>
                 <h3>{he.decode(question.question)}</h3>
-               
+            
                 <div className="answers">
-                    {answers.map((answer, aIndex) => (
-                        <label key={aIndex} className='default'>
+                    {question.answers.map((answer, aIndex) => (
+                        <label 
+                            key={aIndex}
+                            className={`default ${
+                                checked
+                                    ? answer === question.correct_answer
+                                        ? "correct"
+                                        : userAnswers[question.question] === answer
+                                        ? "wrong"
+                                        : "disabled"
+                                    : ""
+                            }`}
+                            onClick={() => !checked && saveUserAnswers(question.question, answer)}
+                        >
                             <input
                                 type="radio"
                                 name={`question-${qIndex}`}
                                 value={answer}
+                                disabled={checked}
                             />
 
                             {he.decode(answer)}
@@ -44,16 +65,48 @@ export default function Questions(props) {
         )
     })
 
+    function saveUserAnswers(question, answer) {
+        setUserAnswers(prev => ({
+            ...prev,
+            [question]: answer
+        }))
+    }
+
+    const allAnswered = Object.keys(userAnswers).length === questions.length
+
+    function test() {
+        setChecked(true)
+    }
+
     return (
         <section className='questions'>
             <div className='questions-wrapper'>
                 {generateQuestions}
             </div>
-            <Button 
-                clickHandler={props.onPrev} 
-                className={btnClassName} 
-                value={btnValue} 
-            />
+
+            {!checked ?
+                <Button 
+                    clickHandler={test} 
+                    className={btnClassName} 
+                    value={btnCheckAnswersValue}
+                    disabled={allAnswered ? null : 'disabled'} 
+                />
+            :   
+                <>
+                    <div className='score-wrapper'>
+                        {checked && (
+                        <h3 className="score">
+                            You scored {score}/{questions.length} correct answers
+                        </h3>
+                        )}
+                        <Button 
+                            clickHandler={props.onPrev} 
+                            className={btnClassName} 
+                            value={btnPlayValue} 
+                        />
+                    </div>
+                </>
+            }
         </section>
     )
 }
--- src/components/buttons/Button.jsx
+++ src/components/buttons/Button.jsx
@@ -4,7 +4,8 @@ export default function Button(props) {
     return (
         <button 
             onClick={props.clickHandler} 
-            className={props.className} 
+            className={props.className}
+            disabled={props.disabled}
         >
             {props.value}
         </button>
